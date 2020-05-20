ece6ca93536c830ad4762d00e825097ef1d2d0d2
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-20 20:55:41 +0300

    Generate all components and added router switch

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
-import Navbar from './components/navbar/Navbar';
 
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
+
+import Navbar from './components/navbar/Navbar';
+import Projects from './components/projects/Projects';
+import Skills from './components/skills/Skills';
+import Home from './components/home/Home';
+import About from './components/about/About';
+import Contacts from './components/contacts/Contacts';
 
 const styles = {
   height: window.innerHeight,
@@ -19,6 +24,23 @@ function App() {
     <div style={styles}>
       <Router>
         <Navbar />
+        <Switch>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+          <Route path="/skills">
+            <Skills />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
