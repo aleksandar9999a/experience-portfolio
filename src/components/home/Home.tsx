34dd04e676c3642f4d8c9c20accc499ee8dec378
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-20 21:43:50 +0300

    Added headline

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import './Home.css';
+
+console.log();
 
 function Home() {
-  return (
-    <div>
-        Home
-    </div>
-  );
+    return (
+        <div className="home">
+            <div className="headline">
+                <h1 className="headline-top">
+                    Hello friends, 
+                    <br/>
+                    My name is Alexandar Todorov.
+                </h1>
+                <hr/>
+                <h2 className="headline-bottom">
+                    Front-End Developer
+                </h2>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
