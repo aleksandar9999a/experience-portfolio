ea49671bd7598c2cc4b6560e5c09c2e121ca59d9
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-21 14:00:56 +0300

    Added base layout and styles

--- src/components/contacts/Contacts.tsx
+++ src/components/contacts/Contacts.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Contacts.css';
 
 function Contacts() {
   return (
-    <div>
-        Contacts
+    <div className="contacts">
+      <div className="title">
+        <h1 className="title-text">Contact with me!</h1>
+      </div>
+      <form className="contact-form">
+        <input className="custom-input" type="text" placeholder="Name"/>
+        <input className="custom-input" type="email" placeholder="Email"/>
+        <input className="custom-input" type="text" placeholder="Subject"/>
+        <textarea className="custom-textarea" placeholder="Message"/>
+        <button className="custom-button">Send</button>
+      </form>
     </div>
   );
 }
