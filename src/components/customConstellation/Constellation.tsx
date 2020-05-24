0a4db252ceb9f43c7ec3504591efd5257c840c33
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-24 16:19:11 +0300

    Clean code, remove setInitialPosition and setCanvas, fix width

--- src/components/customConstellation/Constellation.tsx
+++ src/components/customConstellation/Constellation.tsx
@@ -11,27 +11,15 @@ export default class Constellation {
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas;
         this.context = canvas.getContext("2d");
-    }
-
-    private setCanvas() {
         this.canvas.width = this.constellation_config.width;
         this.canvas.height = this.constellation_config.height;
-    };
+    }
 
     private setContext() {
         if (!this.context) { return; }
         this.context.fillStyle = this.constellation_config.star.color;
         this.context.strokeStyle = this.constellation_config.line.color;
         this.context.lineWidth = this.constellation_config.line.width;
-    };
-
-    private setInitialPosition() {
-        if (!this.constellation_config || !this.constellation_config.hasOwnProperty('position')) {
-            this.constellation_config.position = {
-                x: this.canvas.width * 0.5,
-                y: this.canvas.height * 0.5
-            };
-        }
     };
 
     private loop(callback: any) {
@@ -41,17 +29,16 @@ export default class Constellation {
 
     private subscribeForResize(star: Star) {
         window.addEventListener('resize', () => {
-            this.canvas.width = window.innerWidth;
+            this.canvas.width = window.innerWidth - 20;
             this.canvas.height = window.innerHeight;
             this.setContext();
             star.setWindowSize(window.innerWidth, window.innerHeight);
         })
     }
+    
     init() {
         if (!this.context) { return; }
-        this.setCanvas();
         this.setContext();
-        this.setInitialPosition();
         const star = this.createStar(this.context, this.canvas.width, this.canvas.height);
         star.create();
         this.subscribeForResize(star);
