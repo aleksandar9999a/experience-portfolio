dc20d2105cda74f1ad869dda703adf08208315e6
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-25 17:24:16 +0300

    Added skillsResume, styles

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Skills.css';
+
+const skillsResume = `
+My field of work is in Front-End development or in other words client side of the web.
+I have experience in developing small and medium projects with the help of Angular, React and VueJS.
+I also have experience with Vanilla JS.
+`
 
 function Skills() {
   return (
-    <div>
-        Skills
+    <div className="skills">
+      <div className="skills-title">
+        <h1 className="skills-title-text">Skills</h1>
+        <p className="skills-title-headline">{skillsResume}</p>
+      </div>
     </div>
   );
 }
