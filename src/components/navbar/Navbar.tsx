64c21c0f17cf771799b83c0fe7147de4a315df4c
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 18:08:45 +0300

    Refactoring

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Navbar.css';
 import TabI from '../../interfaces/Tab';
 import * as Icons from 'react-bootstrap-icons';
 import Tab from '../tab/Tab';
+import { getUserInfo } from '../../services/db';
 
-const tabs: TabI[] = [
-    { name: 'Projects', route: '/projects', icon: Icons.ColumnsGap },
-    { name: 'Skills', route: '/skills', icon: Icons.CodeSlash },
-    { name: 'Home', route: '/', icon: Icons.House },
-    { name: 'About', route: '/about', icon: Icons.Person },
-    { name: 'Contacts', route: '/contacts', icon: Icons.ChatDots },
-]
-
-const generateTab = (tab: TabI, i: number) => <Tab data={tab} key={i}/>;
-const list = tabs.map(generateTab);
 
 function Navbar() {
+    const [tabs, setTabs] = useState<JSX.Element[]>();
+    
+    function loadTabList(tabsConfig: TabI[]) {
+        const generateTab = (tab: TabI, i: number) => <Tab data={tab} key={i}/>;
+        const list = tabsConfig.map(generateTab);
+        setTabs(list);
+    }
+
+    useEffect(() => {
+        let tabsConfig: TabI[] = [
+            { name: 'Projects', route: '/projects', icon: Icons.ColumnsGap },
+            { name: 'Skills', route: '/skills', icon: Icons.CodeSlash },
+            { name: 'Home', route: '/', icon: Icons.House },
+            { name: 'About', route: '/about', icon: Icons.Person },
+            { name: 'Contacts', route: '/contacts', icon: Icons.ChatDots },
+        ]
+    
+        const userdata = getUserInfo().subscribe(user => {
+            if (user) {
+                const secretTabs = [
+                    { name: 'Settings', route: '/setting', icon: Icons.Gear },
+                    { name: 'Log Out', route: '/logout', icon: Icons.ArrowBarRight }
+                ]
+                tabsConfig = tabsConfig.concat(secretTabs);
+            }
+
+            loadTabList(tabsConfig);
+        });
+
+        return () => {
+            userdata.unsubscribe();
+        }
+    }, [])
+
+
     return (
         <div className="navbar">
-            <ul className="tabs">{list}</ul>
+            <ul className="tabs">{tabs}</ul>
         </div>
     );
 }
