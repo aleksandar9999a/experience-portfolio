f322bb2009faadc2a15ea221292050f8781e30e2
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 14:01:07 +0300

    Added baseURL as axios default url

--- src/services/db.tsx
+++ src/services/db.tsx
@@ -2,18 +2,20 @@ import config from './../configs/dbConfig';
 import axios from 'axios-observable';
 import baseAxios from 'axios';
 
+axios.defaults.baseURL = `${config.port}/api`;
+
 export function getAbout() {
-    return axios.get(`${config.port}/api/about`, config.credentials);
+    return axios.get(`/about`, config.credentials);
 }
 
 export function getSkills() {
-    return axios.get(`${config.port}/api/skills`, config.credentials);
+    return axios.get(`/skills`, config.credentials);
 }
 
 export function getMainInfo() {
-    return axios.get(`${config.port}/api/auth/`, config.credentials)
+    return axios.get(`/auth/`, config.credentials)
 }
 
 export function submitLogin(loginData: { email: string; password: string; }) {
-    return baseAxios.post(`${config.port}/api/auth/login`, loginData, config.credentials)
+    return baseAxios.post(`/auth/login`, loginData, config.credentials)
 }
