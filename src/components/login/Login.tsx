09a942b107f1a6f924c6d79db37e082bd382cf03
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 12:35:19 +0300

    Added inputs and button

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import './Login.css';
 
 function Login() {
-
     return (
-        <div>
-            login
+        <div className="login">
+            <div className="title">
+                <h1 className="title-text">Login</h1>
+            </div>
+            <form className="contact-form">
+                <input className="custom-input" type="email" placeholder="Email" />
+                <input className="custom-input" type="password" placeholder="Password" />
+                <button className="custom-button">Login</button>
+            </form>
         </div>
     );
 }
