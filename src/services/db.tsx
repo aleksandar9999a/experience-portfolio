cda310f191091d26d8dccab746d1f36b0ca4dae2
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 18:07:23 +0300

    Added interceptors, types, logout, getUserInfo, change getMainInfo

--- src/services/db.tsx
+++ src/services/db.tsx
@@ -1,19 +1,40 @@
 import config from './../configs/dbConfig';
 import axios from 'axios-observable';
 import baseAxios from 'axios';
+import DBAboutResponse from '../interfaces/DBAboutResponse';
+import DBSkillsResponse from '../interfaces/DBSkillsResponse';
+import DBMainUserResponse from '../interfaces/DBMainUserResponse';
+import DBUserResponse from '../interfaces/DBUserResponse';
 
 axios.defaults.baseURL = `${config.port}/api`;
+axios.interceptors.response.use(function (res) {
+    if (res.data === '') {
+        res.data = null;
+    }
+    return res.data;
+  }, function (err) {
+    return Promise.reject(err);
+  });
+
 
 export function getAbout() {
-    return axios.get(`/about`, config.credentials);
+    return axios.get<DBAboutResponse>(`/about`, config.credentials);
 }
 
 export function getSkills() {
-    return axios.get(`/skills`, config.credentials);
+    return axios.get<DBSkillsResponse>(`/skills`, config.credentials);
 }
 
 export function getMainInfo() {
-    return axios.get(`/auth`, config.credentials)
+    return axios.get<DBMainUserResponse>(`/auth/main`, config.credentials);
+}
+
+export function getUserInfo() {
+    return axios.get<DBUserResponse>(`/auth`, config.credentials);
+}
+
+export function logout() {
+    return axios.get(`/auth/logout`, config.credentials);
 }
 
 export function submitLogin(loginData: { email: string; password: string; }) {
