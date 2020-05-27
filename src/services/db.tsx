cf1091f9b223597bf60553a0fb7f57b0c30355b0
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 11:52:35 +0300

    Change urls

--- src/services/db.tsx
+++ src/services/db.tsx
@@ -2,11 +2,11 @@ import config from './../configs/dbConfig';
 import axios from 'axios-observable';
 
 export function getAbout() {
-    return axios.get(`${config.port}/api/pages/about`, config.credentials);
+    return axios.get(`${config.port}/api/about`, config.credentials);
 }
 
 export function getSkills() {
-    return axios.get(`${config.port}/api/pages/skills`, config.credentials);
+    return axios.get(`${config.port}/api/skills`, config.credentials);
 }
 
 export function getMainInfo() {
