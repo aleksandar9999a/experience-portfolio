e02bf0c4421fdc9cdfaca36c13670fa645cbf3b1
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-27 18:39:01 +0300

    Added redirect after logout

--- src/components/logout/Logout.tsx
+++ src/components/logout/Logout.tsx
@@ -1,8 +1,18 @@
+import React, { useState, useEffect } from 'react';
 import { logout } from './../../services/db';
+import { Redirect } from 'react-router-dom';
 
 function Logout() {
-    logout();
+    const [redirect, setRedirect] = useState<boolean>(false);
+
+    useEffect(() => {
+        logout().then(() => setRedirect(true));
+    })
     
+    
+    if (redirect) {
+        return <Redirect to="/" />;
+    }
     return null;
 }
 
