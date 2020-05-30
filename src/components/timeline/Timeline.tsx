982937a4840854f709b766554363b179cd48dda8
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-30 15:58:33 +0300

    Added new props, interface, handlers

--- src/components/timeline/Timeline.tsx
+++ src/components/timeline/Timeline.tsx
@@ -1,27 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Timeline.css';
 import TimelineItemsInterface from '../../interfaces/TimelineItemInterface';
 import TimelineItem from './TimelineItem';
-import TimelineSubmit from '../../interfaces/TimelineSubmit.interface';
+import TimelineAddItem from './TimelineAddItem';
 
 
-function Timeline(props: { items: TimelineItemsInterface[], isEditable?: boolean, onChange?: Function}) {
+function Timeline(props: { items: TimelineItemsInterface[], isEditable?: boolean, onChange?: Function }) {
     const isEditable = props.isEditable || false;
     const onChange = props.onChange;
-    const generateItems = (item: TimelineItemsInterface, i: number) => (<TimelineItem item={item} index={i} key={i} isEditable={isEditable} onSubmit={handleSubmit}/>)
-    const listOfItems = props.items.map(generateItems);
-    let newItems = [...props.items];
+    let [items, setItems] = useState([...props.items]);
+    let [list, setList] = useState<JSX.Element[]>([]);
 
-    function handleSubmit(change: TimelineSubmit) {
-        newItems[change.index] = change.data;
-        if (onChange) {
-            onChange(newItems);
+    useEffect(updateList, [items])
+
+    function updateList() {
+        const generateItems = (item: TimelineItemsInterface) => (<TimelineItem item={item} id={item.id} key={item.id.toString()} isEditable={isEditable} onSubmit={handleSubmit} onRemove={handleRemove} />)
+        setList(items.map(generateItems));
+    }
+
+    function handleSubmit(data: TimelineItemsInterface) {
+        const newItems = [...items];
+        if (data.id === undefined) {
+            let id = 1;
+            if (items.length > 0) {
+                id = items[items.length - 1].id + 1
+            }
+            data.id = id;
+            newItems.push(data);
+        } else {
+            newItems[data.id] = data;
         }
+        if (onChange) { onChange(newItems); }
+        setItems(newItems);
+    }
+
+    function handleRemove(id: number) {
+        setItems(items.filter(x => x.id !== id));
     }
 
     return (
         <ul className="timeline">
-            {listOfItems}
+            {list}
+            {isEditable ? <TimelineAddItem onClick={handleSubmit} /> : null}
         </ul>
     );
 }
