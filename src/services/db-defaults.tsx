5ed8eba0a90473f7de22dc2a39ea0f1db53e3d20
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-30 23:36:05 +0300

    Remove interceptors

--- src/services/db-defaults.tsx
+++ src/services/db-defaults.tsx
@@ -2,14 +2,6 @@ import Axios from "axios-observable";
 import config from './../configs/dbConfig';
 
 Axios.defaults.baseURL = `${config.port}/api`;
-Axios.interceptors.response.use(function (res) {
-    if (res.data === '') {
-        res.data = null;
-    }
-    return res.data;
-}, function (err) {
-    return Promise.reject(err);
-});
 
 export function getAbout() {
     return Axios.get(`/defaults/about`, config.credentials);
