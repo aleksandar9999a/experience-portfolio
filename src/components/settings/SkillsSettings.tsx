640581e98c84459c7b8eb00b2aa0b6a763a258ba
aleksandar9999a <aleksandar9999a@gmail.com>
2020-05-30 15:59:18 +0300

    Added timeline and handlers

--- src/components/settings/SkillsSettings.tsx
+++ src/components/settings/SkillsSettings.tsx
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Settings.css';
+import { getSkills } from './../../services/db';
+import TimelineItemsInterface from '../../interfaces/TimelineItemInterface';
+import Timeline from '../timeline/Timeline';
 
 function SkillsSettings() {
+    const [description, setDescription] = useState<string>();
+    const [timelineItems, setTimelineItems] = useState<TimelineItemsInterface[]>([]);
+
+    useEffect(() => {
+        const skillsData = getSkills().subscribe((data: any) => {
+            setDescription(data.description);
+            setTimelineItems(data.experience);
+        });
+        return () => {
+            skillsData.unsubscribe();
+        }
+    }, [])
+
+    function handleDescriptionChange(e: any) {
+        setDescription(e.target.value);
+    }
+
+    function handleTimelineChange(items: TimelineItemsInterface[]) {
+        // console.log(items);
+
+    }
+
     return (
         <div className="settings-title-wrapper">
             <h1 className="settings-title">Skills</h1>
             <form className="contact-form">
-                <textarea className="custom-textarea" placeholder="Skills Resume" />
+                <textarea className="custom-textarea" placeholder="Skills Resume"
+                    value={description} onChange={handleDescriptionChange} />
+                <div className="settings-skills-timeline">
+                    { timelineItems.length > 0
+                    ? <Timeline items={timelineItems} isEditable={true} onChange={handleTimelineChange} />
+                    : null}
+                </div>
                 <div className="settings-submit-button-wrapper">
                     <button className="settings-submit-button">Update Skills</button>
                 </div>
