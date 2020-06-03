47bb914cac0bd59b44805f333ab62041ad6b2b66
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-03 15:03:12 +0300

    Added getProjects

--- src/services/db-defaults.tsx
+++ src/services/db-defaults.tsx
@@ -13,6 +13,10 @@ export function getSkills() {
     return Axios.get(`/defaults/skills`);
 }
 
+export function getProjects() {
+    return Axios.get(`/defaults/projects`);
+}
+
 export function getMainInfo() {
     return Axios.get(`/defaults`);
 }
