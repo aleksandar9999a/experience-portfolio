1890c7ee78eebe2c20b5326144f23fca8fd21d6c
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-03 15:03:50 +0300

    Render projects

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getProjects } from '../../services/db-defaults';
+import IRProject from '../../interfaces/IRProject';
+import ProjectTile from '../../components/ProjectTile';
+import './styles.css';
 
 function Projects() {
+  let [projects, setProjects] = useState<JSX.Element[]>([]);
+
+  useEffect(() => {
+      getProjects().then(({ data }: { data: IRProject[]}) => {
+          const list = data.map(project => (<div key={project._id} className="project-wrapper"><ProjectTile project={project}/></div>))
+          setProjects(list);
+      }).catch(console.error)
+  }, [])
   return (
     <div>
-        Projects
+        <h1 className="projects-title">Projects</h1>
+        <div className="projects-list">
+          {projects}
+        </div>
     </div>
   );
 }
