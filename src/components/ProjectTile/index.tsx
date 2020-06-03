cf29e1871c2b529ecc9a4ad4a011523a3078e361
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-03 14:34:48 +0300

    Added project tile

---
+++ src/components/ProjectTile/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './styles.css';
+import { Link } from 'react-router-dom';
+import IRProject from '../../interfaces/IRProject';
+
+function ProjectTile({ project }: { project: IRProject }) {
+    return (
+        <Link className="project-tile" to={`projects/${project._id}`}>
+            <img src={project.images[0].url} className="project-tile-preview-img" alt="preview" />
+            <div className="project-tile-preview">
+                <p>{project.title} </p>
+            </div>
+        </Link>
+    );
+}
+
+export default ProjectTile;
