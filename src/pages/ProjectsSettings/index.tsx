0217ccf88a69c2cd6b577fe6dee0dd7b5ed90cde
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-03 14:35:23 +0300

    Render all projects

--- src/pages/ProjectsSettings/index.tsx
+++ src/pages/ProjectsSettings/index.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './styles.css';
 import { Plus } from 'react-bootstrap-icons';
 import { Link } from 'react-router-dom';
+import { getProjects } from '../../services/db-user';
+import IRProject from '../../interfaces/IRProject';
+import ProjectTile from '../../components/ProjectTile';
 
 function ProjectsSettings() {
+    let [projects, setProjects] = useState<JSX.Element[]>([]);
+
+    useEffect(() => {
+        getProjects().then(({ data }: { data: IRProject[]}) => {
+            const list = data.map(project => (<div key={project._id} className="project-wrapper"><ProjectTile project={project}/></div>))
+            setProjects(list);
+        }).catch(console.error)
+    }, [])
+
     return (
         <div className="projects-settings-title-wrapper">
             <h1 className="projects-settings-title">Projects</h1>
             <div className="projects-form">
-                <div className="projetcs-tiles">
+                <div className="projects-tiles">
+                    {projects}
                     <Link className="projects-tile" to="/settings/projects/createProject">
                         <Plus className="projects-add-icon"/>
                     </Link>
