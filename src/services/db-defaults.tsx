ed0a09b19ba0cdad70222e86b13bbf8ebe08f9d0
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-05 02:18:09 +0300

    Added getProjectByID

--- src/services/db-defaults.tsx
+++ src/services/db-defaults.tsx
@@ -17,6 +17,10 @@ export function getProjects() {
     return Axios.get(`/defaults/projects`);
 }
 
+export function getProjectByID(id: string) {
+    return Axios.get(`/defaults/projects/${id}`);
+}
+
 export function getMainInfo() {
     return Axios.get(`/defaults`);
 }
