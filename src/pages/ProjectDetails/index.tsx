1d0a5965a1436640f38c4b88a07ad6172e8013b0
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-05 02:18:32 +0300

    Added new page - Project Details

---
+++ src/pages/ProjectDetails/index.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import './styles.css';
+import { useParams, Link } from 'react-router-dom';
+import { getProjectByID } from '../../services/db-defaults';
+import IRProject from './../../interfaces/IRProject';
+import { auth } from './../../services/db-auth';
+import ProjectDetailsBasic from '../../containers/ProjectDetailsBasic';
+
+function ProjectDetails() {
+    let { id } = useParams();
+    let [project, setProject] = useState<IRProject>();
+    let [isAuth, setIsAuth] = useState<boolean>(false);
+    let [user, setUser] = useState<any>();
+
+    useEffect(() => {
+        getProjectByID(id).then(({ data }: { data: IRProject }) => {
+            setProject(data);
+        }).catch(console.error);
+        const subscriber = auth.subscribe(u => setUser(u));
+
+        return () => {
+            subscriber.unsubscribe();
+        }
+    }, [])
+
+    useEffect(() => {
+        if (!!user && !!project) {
+            setIsAuth(user._id === project.creatorId);
+        } else {
+            setIsAuth(false);
+        }
+    }, [project, user])
+
+    if (!project) { return <div className="project-error"><p className="project-error-text">No data!</p></div> }
+
+    if (isAuth) {
+        return (
+            <div className="project-details">
+                <ProjectDetailsBasic _id={project._id} title={project.title} description={project.description} images={project.images} link={project.link} />
+                <div className="project-actions">
+                    <Link className="link-button" to={`/projects/delete/${project._id}`}>
+                        Edit
+                    </Link>
+                    <Link className="link-button" to={`/projects/edit/${project._id}`}>
+                        Delete
+                    </Link>
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="project-details">
+                <ProjectDetailsBasic _id={project._id} title={project.title} description={project.description} images={project.images} link={project.link} />
+            </div>
+        )
+    }
+
+
+}
+
+export default ProjectDetails;
