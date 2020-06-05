dd4586b26817623037e47b0ee7b75a0c094a5b1f
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-05 14:00:07 +0300

    Added new route for project details

--- src/configs/settingsLinks.tsx
+++ src/configs/settingsLinks.tsx
@@ -3,8 +3,10 @@ import ProjectsSettings from "../pages/ProjectsSettings";
 import SkillsSettings from "../pages/SkillsSettings";
 import AboutSettings from "../pages/AboutSettings";
 import MainInfoSettings from "../pages/MainInfoSettings";
+import ProjectDetails from "../pages/ProjectDetails";
 
 const settingsLinks = [
+    { path: '/settings/projects/:id', Component: ProjectDetails },
     { path: '/settings/projects/createProject', Component: CreateProject },
     { path: '/settings/projects', Component: ProjectsSettings },
     { path: '/settings/skills', Component: SkillsSettings },
