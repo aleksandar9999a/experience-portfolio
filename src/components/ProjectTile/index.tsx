7e21647e08cf3db51c163b566d62063dc3943eb8
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-05 13:59:49 +0300

    Added basicRoute

--- src/components/ProjectTile/index.tsx
+++ src/components/ProjectTile/index.tsx
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 import IProject from '../../interfaces/IProject';
 import IProjectImage from '../../interfaces/IProjectImage';
 
-function ProjectTile({ project }: { project: IProject }) {
+function ProjectTile({ project, basicRoute }: { project: IProject, basicRoute: string }) {
     return (
-        <Link className="project-tile" to={`projects/details/${project._id}`}>
+        <Link className="project-tile" to={`${basicRoute}/${project._id}`}>
             <img src={(project.images as IProjectImage[])[0].url} className="project-tile-preview-img" alt="preview" />
             <div className="project-tile-preview">
                 <p>{project.title} </p>
