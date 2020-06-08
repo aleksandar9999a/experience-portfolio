2b8dc09e3aec0c9703cff796d42e78f6557224e8
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-08 10:48:54 +0300

    Remove IProjectImage

--- src/components/ProjectTile/index.tsx
+++ src/components/ProjectTile/index.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 import './styles.css';
 import { Link } from 'react-router-dom';
 import IProject from '../../interfaces/IProject';
-import IProjectImage from '../../interfaces/IProjectImage';
 
 function ProjectTile({ project, basicRoute }: { project: IProject, basicRoute: string }) {
     return (
         <Link className="project-tile" to={`${basicRoute}/${project._id}`}>
-            <img src={(project.images as IProjectImage[])[0].url} className="project-tile-preview-img" alt="preview" />
+            <img src={(project.images)[0].url} className="project-tile-preview-img" alt="preview" />
             <div className="project-tile-preview">
                 <p>{project.title} </p>
             </div>
