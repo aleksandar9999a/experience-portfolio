ee986bf797b1e55b2aadafcc98fe459d0ec02219
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-10 16:22:08 +0300

    Added footer

--- src/containers/Overlay/index.tsx
+++ src/containers/Overlay/index.tsx
@@ -3,15 +3,17 @@ import './styles.css';
 import { BrowserRouter as Router } from "react-router-dom";
 import Navbar from '../../components/Navbar';
 import Container from '../Container';
+import Footer from '../../components/Footer';
 
 function Overlay() {
   return (
-      <div className="overlay">
-        <Router>
-          <Navbar />
-          <Container />
-        </Router>
-      </div>
+    <div className="overlay">
+      <Router>
+        <Navbar />
+        <Container />
+        <Footer />
+      </Router>
+    </div>
   );
 }
 
