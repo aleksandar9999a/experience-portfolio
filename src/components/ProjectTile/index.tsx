ec65af43b86769914d3ae3c588ac689acd446009
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-10 10:26:17 +0300

    Fix project tile styles

--- src/components/ProjectTile/index.tsx
+++ src/components/ProjectTile/index.tsx
@@ -7,8 +7,8 @@ function ProjectTile({ project, basicRoute }: { project: IProject, basicRoute: s
     return (
         <Link className="project-tile" to={`${basicRoute}/${project._id}`}>
             <img src={(project.images)[0].url} className="project-tile-preview-img" alt="preview" />
-            <div className="project-tile-preview">
-                <p>{project.title} </p>
+            <div className="project-tile-text-wrapper">
+                <p className="project-tile-preview-text">{project.title}</p>
             </div>
         </Link>
     );
