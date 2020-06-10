95a8c3c56d68afebff3fd5f2f27e68527a0e43e4
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-10 16:21:26 +0300

    Added basic footer

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+import Logo from '../Logo';
+
+function Footer() {
+    return (
+        <div className="footer">
+            <Logo width="90px" height="80px" nameSize="14px" logoSize="25px"/>
+        </div>
+    )
+}
+
+export default Footer;
