07140e79a0a65834194a8ad50dc64da2ed3bdff4
aleksandar9999a <aleksandar9999a@gmail.com>
2020-06-11 10:04:42 +0300

    Added logo style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,7 +5,9 @@ import Logo from '../Logo';
 function Footer() {
     return (
         <div className="footer">
-            <Logo width="90px" height="80px" nameSize="14px" logoSize="25px"/>
+            <div className="footer-logo">
+                <Logo width="90px" height="80px" nameSize="14px" logoSize="25px" />
+            </div>
             <p className="footer-text">© 2020 A. Todorov. All rights reserved.</p>
         </div>
     )
