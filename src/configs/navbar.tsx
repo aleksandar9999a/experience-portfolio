9483b267e38471455c13251dde8c1c9ca97bf0f6
aleksandar9999a <aleksandar9999a@gmail.com>
2020-08-08 13:51:54 +0300

    Added id and auth

--- src/configs/navbar.tsx
+++ src/configs/navbar.tsx
@@ -2,13 +2,13 @@ import ITab from "../interfaces/ITab";
 import * as Icons from 'react-bootstrap-icons';
 
 const tabsConfig: ITab[] = [
-    { name: 'Projects', route: '/projects', Icon: Icons.ColumnsGap },
-    { name: 'Skills', route: '/skills', Icon: Icons.CodeSlash },
-    { name: 'Home', route: '/', Icon: Icons.House },
-    { name: 'About', route: '/about', Icon: Icons.Person },
-    { name: 'Contacts', route: '/contacts', Icon: Icons.ChatDots },
-    { name: 'Settings', route: '/settings', Icon: Icons.Gear },
-    { name: 'Log Out', route: '/logout', Icon: Icons.ArrowBarRight }
+    { id: 1, name: 'Projects', route: '/projects', Icon: Icons.ColumnsGap },
+    { id: 2, name: 'Skills', route: '/skills', Icon: Icons.CodeSlash },
+    { id: 3, name: 'Home', route: '/', Icon: Icons.House },
+    { id: 4, name: 'About', route: '/about', Icon: Icons.Person },
+    { id: 5, name: 'Contacts', route: '/contacts', Icon: Icons.ChatDots },
+    { id: 6, name: 'Settings', route: '/settings', Icon: Icons.Gear, auth: true },
+    { id: 7, name: 'Log Out', route: '/logout', Icon: Icons.ArrowBarRight, auth: true }
 ]
 
 export default tabsConfig;
--- src/interfaces/ITab.tsx
+++ src/interfaces/ITab.tsx
@@ -2,7 +2,9 @@ import { ComponentType } from "react";
 import { Props } from "react-bootstrap-icons";
 
 export default interface ITab {
+    id: number,
     name: string,
     route: string,
-    Icon: ComponentType<Props>
+    Icon: ComponentType<Props>,
+    auth?: boolean
 }
