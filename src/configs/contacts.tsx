5ec86c5ef17e7595f3104529381b401f8d148dad
aleksandar9999a <aleksandar9999a@gmail.com>
2020-08-08 16:57:35 +0300

    Extract contacts

---
+++ src/configs/contacts.tsx
@@ -0,0 +1,9 @@
+import IContact from "../interfaces/IContact";
+
+const contacts: IContact[] = [
+    { alt: 'instagram', icon: 'instagram', href: 'https://www.instagram.com/sandi9999a/' },
+    { alt: 'github', icon: 'git', href: 'https://github.com/aleksandar9999a' },
+    { alt: 'linkedin', icon: 'linkedin', href: 'https://www.linkedin.com/in/alexandar-todorov/' }
+]
+
+export default contacts;
