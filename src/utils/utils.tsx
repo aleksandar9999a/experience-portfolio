ded38477975abd9d3d9e4ce6f56e1c2a49b2b415
aleksandar9999a <aleksandar9999a@gmail.com>
2020-08-08 11:22:06 +0300

    Added utils

---
+++ src/utils/utils.tsx
@@ -0,0 +1,32 @@
+
+/**
+ * Generate Command - 'put' or 'post'
+ * 
+ * @param {Object} data
+ * @returns {String} 
+ */
+export function generateCommand(data: any) {
+    return !!data._id
+        ? 'put'
+        : 'post'
+}
+
+/**
+ * Generate ID by date
+ * 
+ * @returns {String}
+ */
+export function generateDateID(str?: string) {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDay();
+    const hour = date.getHours();
+    const minutes = date.getMinutes();
+    const milliseconds = date.getMilliseconds();
+    const customEnd = !!str
+        ? str
+        : '';
+
+    return `${year}${month}${day}${hour}${minutes}${milliseconds}${customEnd}`;
+}
