1311252cbb95f13c4469c2367dc41327ec5e8953
aleksandar9999a <aleksandar9999a@gmail.com>
2020-08-08 16:59:20 +0300

    Refactoring

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -7,30 +7,43 @@ import Error from '../../containers/Error';
 import LoadingPage from '../LoadingPage';
 
 function Projects() {
-  let [projects, setProjects] = useState<JSX.Element[]>([]);
-  let [error, setError] = useState<string>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+    let [projects, setProjects] = useState<IProject[]>([]);
+    let [error, setError] = useState<string>();
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
-  useEffect(() => {
-    getDefaultProjects().then(({ data }: { data: IProject[] }) => {
-      const list = data.map(project => (<div key={project._id} className="project-wrapper"><ProjectTile project={project} basicRoute="/projects/details"/></div>))
-      setProjects(list);
-    }).catch(err => setError(err.message)).finally(() => setIsLoading(false));
-  }, [])
+    useEffect(() => {
+        getDefaultProjects()
+            .then(({ data }: { data: IProject[] }) => {
+                setProjects(data);
+            })
+            .catch(err => setError(err.message))
+            .finally(() => setIsLoading(false));
+    }, [])
 
-  if (isLoading) { return <LoadingPage />; }
-  if (error || projects.length === 0) {  return <Error title="Projects" error={error || 'No Data!'} />}
+    if (isLoading) {
+        return <LoadingPage />;
+    }
 
-  return (
-    <div className="container">
-      <div className="title">
-        <h1>Projects</h1>
-      </div>
-      <div className="projects-list">
-        {projects}
-      </div>
-    </div>
-  );
+    if (error || projects.length === 0) {
+        return <Error title="Projects" error={error || 'No Data!'} />;
+    }
+
+    return (
+        <div className="container">
+            <div className="title">
+                <h1>Projects</h1>
+            </div>
+            <div className="projects-list">
+                {
+                    projects.map(project => {
+                        return <div key={project._id} className="project-wrapper">
+                            <ProjectTile project={project} basicRoute="/projects/details" />
+                        </div>
+                    })
+                }
+            </div>
+        </div>
+    );
 }
 
 export default Projects;
