70a8d817cf544100ef7e0858c083624099a8aacd
aleksandar9999a <aleksandar9999a@gmail.com>
2020-08-08 16:58:05 +0300

    Refactoring

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -13,28 +13,41 @@ function Login() {
 
     function submit(e: SyntheticEvent) {
         e.preventDefault();
-        if (email === '') { setMessage('Email is invalid!'); return; }
+        if (email === '') {
+            setMessage('Email is invalid!');
+            return;
+        }
 
         if (password === '' || password.length < 8 || password.length > 30) {
             setMessage('Password is invalid! Chars must be between 8 and 30.');
             return;
         }
+
         setIsLoading(true)
         submitLogin({ email, password })
             .then(user => {
-                if (user) { setRedirect(true); }
-            }).catch(err => {
+                if (user) {
+                    setRedirect(true);
+                }
+            })
+            .catch(err => {
                 if (err.message === "Request failed with status code 404") {
                     setMessage('Wrong email or password!')
                     return;
                 }
-                console.log(err.message);
-            }).finally(() => setIsLoading(false));
+                console.error(err.message);
+            })
+            .finally(() => setIsLoading(false));
     }
 
     useEffect(() => {
-        const sub = setTimeout(() => { setMessage(''); }, 3000);
-        return () => { clearTimeout(sub); }
+        const sub = setTimeout(() => {
+            setMessage('');
+        }, 3000);
+
+        return () => {
+            clearTimeout(sub);
+        }
     }, [message])
 
     function setEmailValue(e: any) {
@@ -45,8 +58,13 @@ function Login() {
         setPassword(e.target.value);
     }
 
-    if (redirect) { return <Redirect to="/" /> }
-    if (isLoading) { return <LoadingPage /> }
+    if (redirect) {
+        return <Redirect to="/" />;
+    }
+
+    if (isLoading) {
+        return <LoadingPage />;
+    }
 
     return (
         <div className="container">
